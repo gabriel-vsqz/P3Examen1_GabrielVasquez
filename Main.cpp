1a864dae348039135c0708ab03cff3f9ce3a1f80
Gabriel <gabevsqz04@gmail.com>
2020-02-21 14:44:15 -0600

    Menu vBeta

--- Main.cpp
+++ Main.cpp
@@ -14,17 +14,28 @@ Lannister lannister();
 Targaryen targaryen();
 
 int main() {
-	bool pass;
-	int op1, op2;
+	bool pass = true;
+	int op1;
 	while (pass) {
 		cout << "1. Familias\n2. Simulación\n3. Salir\n:";
 		cin >> op1;
 		switch (op1) {
 			case 1: {
+					int op;
+					cout << "\n1. Crear Familia\n2. Crear Soldado\n3. Listar Ejército\n4. Volver\n:";
+					cin >> op;
+					while (op < 1 && op > 4) {
+						cout << "Debe elegir entre una de las opciones indicadas" << endl;
+						cout << "\n1. Crear Familia\n2. Crear Soldado\n3. Listar Ejército\n4. Volver\n:";
+                                        	cin >> op;
+					}
+					switch (op) {
+					}
 				} break;
 			case 2: {
 				} break;
 			case 3: {
+					pass = false;
 				} break;
 			default:
 				cout << "Debe elegir entre una de las opciones indicadas." << endl;
